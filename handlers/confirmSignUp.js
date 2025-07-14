11db8f82c4dc85d2a006b4cd3bfeb1f37c2e0f21
janindujm <janindujm@gmail.com>
2025-07-14 23:21:12 +0530

    Add confirmation for signup

---
+++ handlers/confirmSignUp.js
@@ -0,0 +1,40 @@
+const {CognitoIdentityProviderClient, ConfirmSignUpCommand} = require('@aws-sdk/client-cognito-identity-provider');
+
+
+
+const client = new CognitoIdentityProviderClient({
+    region: 'us-east-2', //Specify the AWS region where your Cognito User Pool is located
+});
+const CLIENT_ID = "5vhi3d3qq9eeqcr9ronjhketuu";
+
+exports.ConfirmSignUp = async (event) => {
+    const {email, confirmationCode} = JSON.parse(event.body);
+
+    const params = {
+        ClientId: CLIENT_ID, //Cognito App Client ID
+        Username: email, //User's email as username
+        ConfirmationCode: confirmationCode
+    }
+
+    try {
+        const command = new ConfirmSignUpCommand(params);
+        await client.send(command);
+
+        return {
+            statusCode: 200, //HTTP status code for successful request
+            body: JSON.stringify({
+                message: "User confirmed successfully", //Success message
+            }),
+        };
+        
+    } catch (error) {
+                return {
+            statusCode: 400, //HTTP status code for internal server error
+            body: JSON.stringify({
+                message: "Confirmation failed", //Error message
+                error: error.message, //Include the error message from the exception
+            }),
+        };
+        
+    }
+}
